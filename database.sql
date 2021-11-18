ba9c8fa869001143f9157f1d0ae111dc17b63e40
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-18 10:18:18 -0600

    added new koalas to the table

--- database.sql
+++ database.sql
@@ -11,4 +11,8 @@ INSERT INTO KoalasNew
 ("NAME", "AGE", "GENDER", "READY FOR TRANSFER", "NOTES")
 VALUES
 ('Scotty', 4, 'M', 'true', 'born in guatemala'),
-('Jean', 5, 'f', 'true', 'allergic to lots of lava');
+('Jean', 5, 'f', 'true', 'allergic to lots of lava'),
+('Ororo', 7, 'f', 'false', 'loves listening to Paula'),
+('Logan', 15, 'm', 'false', 'Love the sauna'),
+('Charlie', 9, 'm', 'true', 'favorite band is Nirvana'),
+('Betsy', 4, 'f', 'true', 'has a pet iguana') 
